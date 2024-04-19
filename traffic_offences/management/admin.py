169173b9bb9376e9dff3072cdd5c8ff3f4c7f728
felixsuarez0727 <felixsuarez0727>
2024-04-19 12:43:10 -0500

    Creating the administrative interface

--- traffic_offences/management/admin.py
+++ traffic_offences/management/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Person, Vehicle, Officer
 
-# Register your models here.
+admin.site.register(Person)
+admin.site.register(Vehicle)
+admin.site.register(Officer)
